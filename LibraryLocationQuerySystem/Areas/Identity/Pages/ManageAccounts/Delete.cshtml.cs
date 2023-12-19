c64f7deb005ead023600bfd30e098529428f0651
tsctsc6@126.com <tsctsc6@126.com>
2023-12-19 22:59:42 +0800

    完成删除reader用户功能

---
+++ LibraryLocationQuerySystem/Areas/Identity/Pages/ManageAccounts/Delete.cshtml.cs
@@ -0,0 +1,37 @@
+using LibraryLocationQuerySystem.Areas.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryLocationQuerySystem.Areas.Identity.Pages.ManageAccounts
+{
+	public class DeleteModel : PageModel
+    {
+		private readonly UserManager<StudentUser> _userManager;
+
+        public DeleteModel(UserManager<StudentUser> userManager)
+        {
+            _userManager = userManager;
+
+		}
+
+		public StudentUser? User { get; private set; } = null;
+
+		public async Task<IActionResult> OnGetAsync(string? StudentId)
+        {
+			if (StudentId == null) return NotFound();
+			User = await _userManager.FindByNameAsync(StudentId);
+			if (User == null) return NotFound();
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync(string? StudentId)
+		{
+			if (StudentId == null) return NotFound();
+			var user = await _userManager.FindByNameAsync(StudentId);
+			if (user == null) return RedirectToPage("./Index");
+			var result = await _userManager.DeleteAsync(user);
+			return RedirectToPage("./Index");
+		}
+	}
+}
