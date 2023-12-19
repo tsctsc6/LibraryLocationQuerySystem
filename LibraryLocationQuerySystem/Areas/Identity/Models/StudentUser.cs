5411905d711918927edafd67bdd11714e0288c2e
tsctsc6@126.com <tsctsc6@126.com>
2023-12-19 19:40:10 +0800

    创建实体的类

---
+++ LibraryLocationQuerySystem/Areas/Identity/Models/StudentUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryLocationQuerySystem.Areas.Identity.Models
+{
+    public class StudentUser : IdentityUser
+    {
+        [Column(TypeName = "char(10)")]
+        [MaxLength(10)]
+        [Required]
+        [RegularExpression(@"\d+")]
+        [PersonalData]
+        public string StudentId { get; set; }
+
+        [Required]
+        public bool IsPasswordChanged { get; set; } = false;
+    }
+}
