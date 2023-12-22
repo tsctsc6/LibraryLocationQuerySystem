4fef537ac7eee9b9729e007e1f5058f8e1b9485a
tsctsc6@126.com <tsctsc6@126.com>
2023-12-22 17:00:46 +0800

    编写lcation Edit

---
+++ LibraryLocationQuerySystem/Pages/Locations/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibraryLocationQuerySystem.Data;
+using LibraryLocationQuerySystem.Models;
+
+namespace LibraryLocationQuerySystem.Pages.Locations
+{
+    public class EditModel : PageModel
+    {
+        private readonly LibraryLocationQuerySystem.Data.StoreManagerDbContext _context;
+
+        public EditModel(LibraryLocationQuerySystem.Data.StoreManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Location Location { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(byte? LocationLevel, byte? LocationId, byte? LocationParent)
+        {
+            if (LocationLevel == null || LocationId == null || LocationParent == null || _context.Location == null)
+            {
+                return NotFound();
+            }
+
+            var location =  await _context.Location.FirstOrDefaultAsync(
+                m => m.LocationLevel == LocationLevel && m.LocationId == LocationParent && m.LocationParent == LocationParent);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            Location = location;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Location).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocationExists(Location.LocationLevel, Location.LocationId, Location.LocationParent))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool LocationExists(byte LocationLevel, byte LocationId, byte LocationParent)
+        {
+            return (_context.Location?.Any(m => m.LocationLevel == LocationLevel &&
+                m.LocationId == LocationParent && m.LocationParent == LocationParent)).GetValueOrDefault();
+        }
+    }
+}
