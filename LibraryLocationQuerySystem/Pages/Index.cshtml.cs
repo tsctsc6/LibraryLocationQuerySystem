93c7639d4a794b3a578ab50b07945f8120d0b348
tsctsc6@126.com <tsctsc6@126.com>
2023-12-22 19:25:21 +0800

    添加更改密码提醒；优化账号管理界面

--- LibraryLocationQuerySystem/Pages/Index.cshtml.cs
+++ LibraryLocationQuerySystem/Pages/Index.cshtml.cs
@@ -1,20 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryLocationQuerySystem.Areas.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LibraryLocationQuerySystem.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly UserManager<StudentUser> _userManager;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(UserManager<StudentUser> userManager,
+            ILogger<IndexModel> logger)
         {
+            _userManager = userManager;
             _logger = logger;
         }
 
-        public void OnGet()
-        {
+        public bool IsPasswordChanged { get; set; } = true;
 
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) return Page();
+            IsPasswordChanged = user.IsPasswordChanged;
+            return Page();
         }
     }
 }
