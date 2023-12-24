891a1437233f15be59826c6d30c84a2605c5b824
tsctsc6@126.com <tsctsc6@126.com>
2023-12-24 20:29:33 +0800

    修改StoreManagerDbContext

--- LibraryLocationQuerySystem/Data/StoreManagerDbContext.cs
+++ LibraryLocationQuerySystem/Data/StoreManagerDbContext.cs
@@ -14,13 +14,14 @@ namespace LibraryLocationQuerySystem.Data
         public DbSet<Store> Store { get; set; } = default!;
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Book>()
-                .HasMany(e => e.Locations)
-                .WithMany(e => e.Books)
-                .UsingEntity<Store>(
-                    l => l.HasOne(e => e.Location).WithMany(e => e.Stores).HasForeignKey("LocationLevel", "LocationId"),
-                    r => r.HasOne(e => e.Book).WithMany(e => e.Stores).HasForeignKey("BookSortCallNumber", "BookFormCallNumber"),
+            modelBuilder.Entity<Book>().HasMany(e => e.Locations)
+                .WithMany(e => e.Books).UsingEntity<Store>(
+                    l => l.HasOne(e => e.Location).WithMany(e => e.Stores)
+                        .HasForeignKey("LocationLevel", "LocationId"),
+                    r => r.HasOne(e => e.Book).WithMany(e => e.Stores)
+                        .HasForeignKey("BookSortCallNumber", "BookFormCallNumber"),
                     j => j.Property(e => e.StoreDate).HasDefaultValueSql("CURRENT_TIMESTAMP"));
+            modelBuilder.Entity<Book>().Property(e => e.Type).HasConversion<byte>();
         }
     }
 }
