d2cc87e8a4fa4df72b4767f3202a87b0909ec5b0
tsctsc6@126.com <tsctsc6@126.com>
2023-12-24 15:54:53 +0800

    在book的详情页可以查看哪里存了这本书

--- LibraryLocationQuerySystem/Pages/Books/Details.cshtml.cs
+++ LibraryLocationQuerySystem/Pages/Books/Details.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LibraryLocationQuerySystem.Data;
 using LibraryLocationQuerySystem.Models;
+using System.Text;
 
 namespace LibraryLocationQuerySystem.Pages.Books
 {
@@ -19,11 +20,12 @@ namespace LibraryLocationQuerySystem.Pages.Books
             _context = context;
         }
 
-      public Book Book { get; set; } = default!; 
+        public Book Book { get; set; } = default!;
+        public string[] LocationPaths { get; set; } = null;
 
         public async Task<IActionResult> OnGetAsync(string? BookSortCallNumber, string? BookFormCallNumber)
         {
-            if (BookSortCallNumber == null || BookFormCallNumber == null || _context.Book == null)
+            if (BookSortCallNumber == null || BookFormCallNumber == null || _context.Book == null || _context.Store == null)
             {
                 return NotFound();
             }
@@ -34,7 +36,34 @@ namespace LibraryLocationQuerySystem.Pages.Books
                 return NotFound();
             }
             Book = book;
+
+            var stores = await _context.Store.Where(s => s.BookSortCallNumber == Book.BookSortCallNumber &&
+                s.BookFormCallNumber == Book.BookFormCallNumber).ToArrayAsync();
+            LocationPaths = await Task.WhenAll(stores.Select(async s => await SetLocationPath(s.LocationLevel, s.LocationId)));
+
             return Page();
+        }
+
+        private async Task<string> SetLocationPath(byte LocationLevel, int LocationId)
+        {
+            if (_context.Location == null) return string.Empty;
+            List<string> strings = new();
+            while (LocationLevel >= 0 && LocationLevel < 5)
+            {
+                var loc = await _context.Location.Where(l => l.LocationLevel == LocationLevel &&
+                    l.LocationId == LocationId).FirstOrDefaultAsync();
+                if (loc == null) throw new ArgumentNullException("Location元组not find");
+                LocationId = loc.LocationParent;
+                LocationLevel--;
+                strings.Insert(0, loc.LocationName);
+            }
+            StringBuilder sb = new("/ ");
+            foreach (var item in strings)
+            {
+                sb.Append(item);
+                sb.Append(" / ");
+            }
+            return sb.ToString();
         }
     }
 }
