9d7c2f802e84e44406cb6c68222230911d2ccaee
tsctsc6@126.com <tsctsc6@126.com>
2024-01-01 17:15:21 +0800

    一次性创建完整的地点信息

--- LibraryLocationQuerySystem/Pages/Locations/Create.cshtml.cs
+++ LibraryLocationQuerySystem/Pages/Locations/Create.cshtml.cs
@@ -4,6 +4,7 @@ using LibraryLocationQuerySystem.Migrations;
 using LibraryLocationQuerySystem.Models;
 using LibraryLocationQuerySystem.Utilities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.CodeAnalysis.Classification;
@@ -14,7 +15,26 @@ namespace LibraryLocationQuerySystem.Pages.Locations
 {
     public class CreateModel : PageModel
     {
-		public class SelectGroupView
+        public class LocationNames
+        {
+            [StringLength(30)]
+            [Display(Name = "校区名")]
+            public string? CampusName { get; set; }
+            [StringLength(30)]
+            [Display(Name = "图书馆/图书室名")]
+            public string? LibraryName { get; set; }
+            [StringLength(30)]
+            [Display(Name = "楼层名")]
+            public string? FloorName { get; set; }
+            [StringLength(30)]
+            [Display(Name = "书架名")]
+            public string? BookshelfName { get; set; }
+            [StringLength(30)]
+            [Display(Name = "书架层名")]
+            public string? LayerName { get; set; }
+        }
+
+        public class SelectGroupView
         {
 			public int CampusId { get; set; }
 			public int LibraryId { get; set; }
@@ -27,14 +47,15 @@ namespace LibraryLocationQuerySystem.Pages.Locations
 
 		[BindProperty(SupportsGet = true)]
 		public SelectGroupView selectGroupView { get; set; } = new();
-		public List<SelectListItem> Campuses { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public LocationNames locationNames { get; set; } = new();
+        public List<SelectListItem> Campuses { get; set; }
 		public List<SelectListItem> Libraries { get; set; }
 		public List<SelectListItem> Floors { get; set; }
 		public List<SelectListItem> Bookshelves { get; set; }
-		//public List<SelectListItem> Layers { get; set; }
+        //public List<SelectListItem> Layers { get; set; }
 
-        [BindProperty]
-        public Location Location { get; set; } = default!;
+        public Location[] Locations { get; } = new Location[5];
 
         public CreateModel(StoreManagerDbContext context)
 		{
@@ -50,23 +71,36 @@ namespace LibraryLocationQuerySystem.Pages.Locations
 		{
             await InitSelectGrop();
             //PrintModelState.printErrorMessage(ModelState);
-            if (!ModelState.IsValid || _context.Location == null || Location == null)
+            if (_context.Location == null)
             {
                 return Page();
             }
-            SetLocationLevelAndParent();
-            do { Location.GenLocationid(); }
-            while (await _context.Location.Where(l => l.LocationId == Location.LocationId).CountAsync() != 0 || Location.LocationId == 0);
-            Location.ManageBy = User?.Identity?.Name;
-            await _context.Location.AddAsync(Location);
+
+            int index;
+            try { index = SetLocationLevelAndParent(); }
+            catch (ArgumentNullException e)
+            {
+                ModelState.AddModelError(string.Empty, e.Message);
+                return Page();
+            }
+            while(index < Locations.Length)
+            {
+                do { Locations[index].GenLocationid(); }
+                while (await _context.Location.Where(l => l.LocationId == Locations[index].LocationId).CountAsync() != 0 || Locations[index].LocationId == 0);
+                if (index > 0) Locations[index].LocationParent = Locations[index - 1].LocationId;
+                Locations[index].ManageBy = User?.Identity?.Name;
+                await _context.Location.AddAsync(Locations[index]);
+                index++;
+            }
+
 			try { await _context.SaveChangesAsync(); }
 			catch (DbUpdateException e)
 			{
 				ModelState.AddModelError(string.Empty, e.InnerException?.Message ?? string.Empty);
 				return Page();
 			}
-
-			return RedirectToPage("./Index");
+            
+            return RedirectToPage("./Index");
         }
 
         private async Task InitSelectGrop()
@@ -94,42 +128,72 @@ namespace LibraryLocationQuerySystem.Pages.Locations
             Layers.Insert(0, new("(未选择)", "0"));
             */
         }
-
-        private void SetLocationLevelAndParent()
+        
+        private int SetLocationLevelAndParent()
         {
-            if (selectGroupView.CampusId == 0)
+            int t = -1;
+            if (selectGroupView.CampusId != 0)
             {
-                Location.LocationLevel = 0;
-                Location.LocationParent = 0;
-                return;
-            }
-            if (selectGroupView.LibraryId == 0)
-            {
-                Location.LocationLevel = 1;
-                Location.LocationParent = selectGroupView.CampusId;
-                return;
-            }
-            if (selectGroupView.FloorId == 0)
-            {
-                Location.LocationLevel = 2;
-                Location.LocationParent = selectGroupView.LibraryId;
-                return;
-            }
-            if (selectGroupView.BookshelfId == 0)
-            {
-                Location.LocationLevel = 3;
-                Location.LocationParent = selectGroupView.FloorId;
-                return;
+                Locations[0] = _context.Location.Single(l => l.LocationLevel == 0 && l.LocationId == selectGroupView.CampusId);
             }
             else
             {
-                Location.LocationLevel = 4;
-                Location.LocationParent = selectGroupView.BookshelfId;
-                return;
+                Locations[0] = new();
+                Locations[0].LocationLevel = 0;
+                Locations[0].LocationParent = 0;
+                if (locationNames.CampusName == null) throw new ArgumentNullException("locationNames.CampusName == null");
+                Locations[0].LocationName = locationNames.CampusName;
+                if (t == -1) t = 0;
             }
+            if (selectGroupView.LibraryId != 0)
+            {
+                Locations[1] = _context.Location.Single(l => l.LocationLevel == 0 && l.LocationId == selectGroupView.LibraryId);
+            }
+            else
+            {
+                Locations[1] = new();
+                Locations[1].LocationLevel = 1;
+                Locations[1].LocationParent = selectGroupView.CampusId;
+                if (locationNames.LibraryName == null) throw new ArgumentNullException("locationNames.LibraryName == null");
+                Locations[1].LocationName = locationNames.LibraryName;
+                if (t == -1) t = 1;
+            }
+            if (selectGroupView.FloorId != 0)
+            {
+                Locations[2] = _context.Location.Single(l => l.LocationLevel == 0 && l.LocationId == selectGroupView.FloorId);
+            }
+            else
+            {
+                Locations[2] = new();
+                Locations[2].LocationLevel = 2;
+                Locations[2].LocationParent = selectGroupView.LibraryId;
+                if (locationNames.FloorName == null) throw new ArgumentNullException("locationNames.FloorName == null");
+                Locations[2].LocationName = locationNames.FloorName;
+                if (t == -1) t = 2;
+            }
+            if (selectGroupView.BookshelfId != 0)
+            {
+                Locations[3] = _context.Location.Single(l => l.LocationLevel == 0 && l.LocationId == selectGroupView.BookshelfId);
+            }
+            else
+            {
+                Locations[3] = new();
+                Locations[3].LocationLevel = 3;
+                Locations[3].LocationParent = selectGroupView.FloorId;
+                if (locationNames.BookshelfName == null) throw new ArgumentNullException("locationNames.BookshelfName == null");
+                Locations[3].LocationName = locationNames.BookshelfName;
+                if (t == -1) t = 3;
+            }
+            Locations[4] = new();
+            Locations[4].LocationLevel = 4;
+            Locations[4].LocationParent = selectGroupView.BookshelfId;
+            if (locationNames.LayerName == null) throw new ArgumentNullException("locationNames.LayerName == null");
+            Locations[4].LocationName = locationNames.LayerName;
+            if (t == -1) t = 4;
+            return t;
         }
         
-		public async Task<JsonResult> OnGetParentAsync(int LocationLevel, int LocationParent)
+        public async Task<JsonResult> OnGetParentAsync(int LocationLevel, int LocationParent)
 		{
             if (_context.Location == null) return new JsonResult("_context.Location == null");
             var res = await _context.Location.Where(l => l.LocationLevel == LocationLevel && l.LocationParent == LocationParent).ToListAsync();
