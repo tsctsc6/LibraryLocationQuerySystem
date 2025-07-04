5d6830e1b0ea9fbe1bec7a44fc782cd41d244926
tsctsc6 <tsctsc6@126.com>
2025-07-04 22:47:18 +0800

    新建迁移

---
+++ LibraryLocationQuerySystem/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace LibraryLocationQuerySystem.Data;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    //ApplicationDbContext 代表的是你的创建失败的那个类型
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .Build();
+        var builder = new DbContextOptionsBuilder();
+        builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        return new ApplicationDbContext(builder.Options);
+    }
+}
